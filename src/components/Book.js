2dd543dc206ae5e56c9bce9c32ced6542e00e7a7
Nikita Malov <nikmalov@gmail.com>
2021-01-16 20:58:06 +0100

    feat: Introduced Book and BookShelveChanger

---
+++ src/components/Book.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import BookShelveChanger from './BookShelveChanger.js';
+
+class Book extends Component {
+
+    render() {
+        const bookCoverStyle = {
+            width: this.props.coverWidth || 128,
+            height: this.props.coverHeight,
+            backgroundImage: `url(${this.props.coverUrl})`
+        }
+
+        return (
+            <div className="book">
+                <div className="book-top">
+                    <div className="book-cover" style={bookCoverStyle}/>
+                    <BookShelveChanger />
+                </div>
+                <div className="book-title">{this.props.title}</div>
+                <div className="book-authors">{this.props.author}</div>
+            </div>
+        )
+    }
+}
+
+export default Book;
